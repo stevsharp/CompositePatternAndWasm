44c88ac7367b1845c31f5a17f6ac6ab8af6a72d9
SPYROS\Spyros <sponaris@gmail.com>
2024-07-17 18:38:47 +0300

    Add MudBlazor Support

--- CompositePatternAndWasm/Program.cs
+++ CompositePatternAndWasm/Program.cs
@@ -3,10 +3,22 @@ using CompositePatternAndWasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using MudBlazor;
+using MudBlazor.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices(configuration =>
+{
+    configuration.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    configuration.SnackbarConfiguration.HideTransitionDuration = 100;
+    configuration.SnackbarConfiguration.ShowTransitionDuration = 100;
+    configuration.SnackbarConfiguration.VisibleStateDuration = 3000;
+    configuration.SnackbarConfiguration.ShowCloseIcon = false;
+});
 
 await builder.Build().RunAsync();
